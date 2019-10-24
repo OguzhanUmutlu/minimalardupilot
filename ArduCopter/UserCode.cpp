4450176a279c0993c861befc7925184ee480379b
Rob Ratcliff <robert.r.ratcliff@boeing.com>
2019-10-24 15:13:54 -0500

    allow TMRS mode to be disabled for non-TMRS platforms

---
+++ ArduCopter/UserCode.cpp
@@ -0,0 +1,78 @@
+// TMRS v1 hooks
+
+#include "Copter.h"
+
+#ifdef USERHOOK_INIT
+void Copter::userhook_init()
+{
+#if TMRS_ENABLED == ENABLED
+    tmrs_motor_power_monitor.init();
+    tmrs_tether_power_monitor.init();
+    tmrs_payload_controller.init();
+#endif
+}
+#endif
+
+#ifdef USERHOOK_FASTLOOP
+void Copter::userhook_FastLoop()
+{
+    // put your 100Hz code here
+}
+#endif
+
+#ifdef USERHOOK_50HZLOOP
+void Copter::userhook_50Hz()
+{
+    // put your 50Hz code here
+}
+#endif
+
+#ifdef USERHOOK_MEDIUMLOOP
+void Copter::userhook_MediumLoop()
+{
+    // put your 10Hz code here
+}
+#endif
+
+#ifdef USERHOOK_SLOWLOOP
+void Copter::userhook_SlowLoop()
+{
+    // put your 3.3Hz code here
+}
+#endif
+
+#ifdef USERHOOK_SUPERSLOWLOOP
+void Copter::userhook_SuperSlowLoop()
+{
+#if TMRS_ENABLED == ENABLED
+    /**
+     * TMRS sensors
+     */
+    uint8_t n = gcs().num_gcs();
+    for(uint8_t i = 0; i < n; i++) {
+        if(gcs().chan(i).initialised) {
+            tmrs_motor_power_monitor.send_mavlink_tmrs_motor_status(gcs().chan(i).get_chan());
+            tmrs_tether_power_monitor.send_mavlink_tether_power_status(gcs().chan(i).get_chan());
+            tmrs_payload_controller.send_mavlink_tmrs_payload_status(gcs().chan(i).get_chan());
+        }
+    }
+#endif
+}
+#endif
+
+#ifdef USERHOOK_AUXSWITCH
+void Copter::userhook_auxSwitch1(uint8_t ch_flag)
+{
+    // put your aux switch #1 handler here (CHx_OPT = 47)
+}
+
+void Copter::userhook_auxSwitch2(uint8_t ch_flag)
+{
+    // put your aux switch #2 handler here (CHx_OPT = 48)
+}
+
+void Copter::userhook_auxSwitch3(uint8_t ch_flag)
+{
+    // put your aux switch #3 handler here (CHx_OPT = 49)
+}
+#endif
---
+++ ArduCopter/UserVariables.h
@@ -0,0 +1,13 @@
+// user defined variables
+
+// example variables used in Wii camera testing - replace with your own variables
+
+#define TMRS_ENABLED DISABLED
+
+#ifdef USERHOOK_VARIABLES
+
+TMRS_Motor_Power_Monitor tmrs_motor_power_monitor;
+TMRS_Payload_Controller tmrs_payload_controller;
+TMRS_Tether_Power_Monitor tmrs_tether_power_monitor;
+
+#endif  // USERHOOK_VARIABLES
